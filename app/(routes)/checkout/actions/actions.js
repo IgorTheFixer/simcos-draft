139040435233371774906f4e0ff9b09735e89c38
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2024-10-09 12:46:45 -0400

    testing skeleton build

--- app/(routes)/checkout/actions/actions.js
+++ app/(routes)/checkout/actions/actions.js
@@ -8,20 +8,74 @@ BigInt.prototype.toJSON = function () {
 };
 
 const { paymentsApi } = new Client({
-  accessToken: "EAAAEHHRUeQlPQ7oagwti7wZnvu2t40i-WOJ7LwLGkoQ_eMF4xEPaLVsBBwPtNun",
+  accessToken: process.env.SQUARE_ACCESS_TOKEN,
   environment: "sandbox",
 });
 
-export async function submitPayment(sourceId) {
+//Making an order to send to POS
+const { ordersApi } = new Client({
+  accessToken: process.env.SQUARE_ACCESS_TOKEN,
+  environment: "sandbox",
+})
+
+//template
+// export async function submitOrder(){
+//   try {
+//     const { result } = await ordersApi.createOrder({
+//       order: {
+//         locationId: 'L26Y90P0YB65A',
+//         lineItems: [
+//           {
+//             name: 'Gyro Sandwich',
+//             quantity: '1',
+//             itemType: 'ITEM',
+//             basePriceMoney: {
+//               amount: 995,
+//               currency: 'USD'
+//             }
+//           }
+//         ]
+//       },
+//       idempotencyKey: randomUUID()
+//     });
+    
+//     return result.order.id
+//   } catch(error) {
+//     console.log(error);
+//   }
+// }
+
+export async function submitOrder(lineItems){
   try {
+    const { result } = await ordersApi.createOrder({
+      order: {
+        locationId: 'L26Y90P0YB65A',
+        lineItems: lineItems
+      },
+      idempotencyKey: randomUUID()
+    });
+    
+    return result.order.id
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+export async function submitPayment(sourceId, cartTotal, lineItems) {
+  try {
+    // const orderId = await createOrderForChickenSub(locationId);
+    const orderId = await submitOrder(lineItems)
+
     const { result } = await paymentsApi.createPayment({
       idempotencyKey: randomUUID(),
       sourceId,
       amountMoney: {
         currency: "USD",
-        amount: 100,
+        amount: cartTotal
       },
+      orderId
     });
+    
     return result;
   } catch (error) {
     console.log(error);
