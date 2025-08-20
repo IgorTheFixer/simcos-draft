f07ad9d3e33d2a037895d652886c7573f9e50180
cijean-p <cjean-p@CIJEAN-P-MBA.local>
2025-08-20 17:45:35 -0500

    WIP

---
+++ app/(routes)/simcos-menu/page.tsx
@@ -0,0 +1,104 @@
+'use client'
+import Container from "@/components/ui/Container";
+
+import Footer from '@/components/Footer';
+import Header from "@/components/Header";
+import { inter } from "@/fonts";
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import Button from "@/components/ui/Button";
+import { SimcosButton } from "@/components/ui/SimcosButton";
+
+
+const HomePage = () => {
+
+  return (
+    <Container>
+      <div className="flex flex-col min-h-screen max-w-full relative overflow-x-hidden">
+        {/* Background Image */}
+        <div 
+          className="absolute w-[500px] h-[375px] md:w-[800px] md:h-[600px] lg:w-[1200px] lg:h-[900px] -z-10"
+          style={{
+            top: '-191px',
+            right: '-261px',
+            backgroundImage: 'url(/simcos-homepage-combined.png)',
+            backgroundSize: 'contain',
+            backgroundPosition: 'top right',
+            backgroundRepeat: 'no-repeat'
+          }}
+        />
+        <Header />
+        {/* Content overlay */}
+        <div className={`flex-1 min-w-full px-8 md:px-40 lg:px-40 md:text-simcosOrange text-5xl font-bold m-8 mb-4 ${inter.className}`}>
+          <div className="border-b-[1px] border-simcosOrange w-fit flex gap-2">
+            MENU
+            <SimcosButton variant={"secondary"} className="bg-simcosOrange text-white font-medium text-[6px] w-24 whitespace-normal text-start leading-tight rounded-tr-[48.5px]">Todays Offer<br/>FREE BOX OF FRIES<br/>On all orders above $150</SimcosButton>
+          </div>
+          {/* Action Buttons */}
+          <div className="flex gap-4">
+            <Button className="bg-white shadow-lg shadow-gray-500 flex flex-col items-center text-left w-32 md:w-40 lg:w-48  rounded-[57px] rounded-br-[142px]">
+              <Image 
+                src={"/location.png"}
+                alt="an icon of a storefront"
+                width={80}
+                height={80}
+                className="aspect-square object-cover rounded-xl"
+                />
+              <p className="text-base md:text-lg lg:text-xl text-simcosOrange mb-4">Curbside<br/>pickup</p>
+            </Button>
+
+            <Button className="bg-white shadow-lg shadow-gray-500 flex flex-col items-center text-left w-32 md:w-40 lg:w-48  rounded-[57px] rounded-br-[142px]">
+              <Image 
+                src={"/pickup-car.png"}
+                alt="an icon of a delivery truck"
+                width={80}
+                height={80}
+                className="aspect-square object-cover rounded-xl"
+                />
+              <p className="text-base md:text-lg lg:text-xl text-simcosOrange mb-4">Delivery</p>
+            </Button>
+
+            <Button className="bg-white shadow-lg shadow-gray-500 flex flex-col items-center text-left w-32 md:w-40 lg:w-48  rounded-[57px] rounded-br-[142px]">
+              <Image 
+                src={"/burger.png"}
+                alt="an icon of a burger"
+                width={80}
+                height={80}
+                className="aspect-square object-cover rounded-xl"
+                />
+              <p className="text-base md:text-lg lg:text-xl text-simcosOrange mb-4">Menu</p>
+            </Button>
+          </div>
+          <Card className="bg-[#D9D9D9] rounded-tl-[167px] rounded-br-[113px]"> 
+            <CardContent className="p-0">
+              <div className="flex flex-col md:flex-row-reverse items-center">
+                <div className="relative w-64 h-64 md:w-80 md:h-80 lg:w-96 lg:h-96 rounded-xl">
+                  <Image 
+                    src={"/two-people.png"}
+                    alt="two individuals having a conversation"
+                    fill
+                    className="aspect-square object-cover rounded-xl rounded-br-[113px]"
+                  />
+                </div>
+                <div className="flex flex-col p-4 md:p-6 lg:p-8 text-center md:text-left">
+                  <p className="text-base md:text-lg lg:text-xl text-simcosOrange mb-4">We would like you to be part of our team!</p>
+                  <Button className="whitespace-nowrap text-sm md:text-base px-4 py-2 md:px-6 md:py-3">Contact us here</Button>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <Footer />
+      </div>
+    </Container>
+  )
+};
+
+export default HomePage;
