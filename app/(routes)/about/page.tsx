b9729088027d2cb1b7c5f3be2576abfc0a9aa041
cijean-p <cjean-p@CIJEAN-P-MBA.local>
2025-08-20 10:10:05 -0500

    finished about page

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -4,6 +4,7 @@ import Container from "@/components/ui/Container";
 
 import Footer from '@/components/Footer';
 import Header from "@/components/Header";
+import { inter } from "@/fonts";
 
 // export const revalidate = 0;
 
@@ -13,7 +14,7 @@ const AboutPage = () => {
     <Container>
       <div className="flex min-h-screen flex-col">
         <Header />
-        <div className="flex-1 min-w-full px-8 md:px-40 lg:px-40">
+        <div className={`flex-1 min-w-full px-8 md:px-40 lg:px-40 md:text-simcosOrange text-5xl font-bold m-8 mb-4 ${inter.className}`}>
           ABOUT
         </div>
         <Footer />
